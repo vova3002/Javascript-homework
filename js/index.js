810e36219421b649d4307b20ae1ccc19899a7ffd
vova3002 <98548237+vova3002@users.noreply.github.com>
2023-10-23 17:37:02 +0300

    complete hw11

--- js/index.js
+++ js/index.js
@@ -1,70 +1,66 @@
-let num = 0;
+const array = [9, 2, 3];
+array[1] = 10
+console.log(array)
 
-while (num < 11) {
-  console.log(num);
-  num += 1;
-}
-// 1 ^
-// 2 v
+// // 1 ^
+// // 2 v
 
-const num2 = 2
-for (let i = 0; i <= 20; i += 1) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+const array2 = ['name', 'name2', 'name3']
+array2.push('name4')
+console.log(array2)
 
 // 2 ^
 // 3 v
 
-for (let a = 1; a <= 10; a += 1) {
-  console.log(`${a} * 7 = ${a * 7}`);
+const array3 = [3, 11, 28, 89]
+let totalSum = 0
+
+for (let i = 0; i < array3.length; i += 1){
+  totalSum = totalSum + array3[i]
 }
+console.log(totalSum)
 
 // 3 ^
 // 4 v
 
-const mass = ["Apple", "Orange", "Pear", "Banana", "Cherry"];
-
-for (let i = 0; i < mass.length; i += 1) {
-  console.log(mass[i]);
+const array4 = [9, 10, 11, 12, 13]
+for(let i = 0; i < array4.length; i += 1) {
+  alert(`${array4[i]}`)
+  console.log(array4)
 }
 
 // 4 ^
 // 5 v
 
-for (let c = 1; c <= 10; c += 1) {
-  if (c === 7) {
-    break;
-  } else {
-    console.log(c);
+const array5 = ["mango", "pear", "apple", "tomato", "brocoli"]
+for(let i = 0; i < array5.length; i += 1){
+  if(array5[i].length > 4){
+    alert(`${array5[i]}`)
   }
+  console.log(array5)
 }
 
 // 5 ^
 // 6 v
 
-let n = prompt('Введіть число');
-
-for (let f = 1; f <= n; f += 1) {
-  if (f === n) {
-    break;
-  } else {
-    console.log(f);
+const array6 = [9, 10, 21, 72, 81, 23, 53, 7, 8, 9, 1210]
+let maxNum = array6[0]
+for(let i = 0; i < array6.length; i += 1){
+  if(array6[i] > maxNum) {
+    maxNum = array6[i]
   }
 }
+console.log(maxNum)
 
 // 6 ^
 // 7 v
 
-const number = 20;
-
-for (let h = 0; h < number; h += 1) {
-  if (h % 2 ===0) {
-    continue;
-  } else {
-    console.log(h)
+const array7 = [2, 4, 6, 8, 10, 11, 13, 15, 17, 19]
+for(let i = 0; i < array7.length; i += 1){
+  if(array7[i] %2 === 0) {
+    alert(`${array7[i]}`)
   }
 }
+console.log(array7)
 
 // 7 
