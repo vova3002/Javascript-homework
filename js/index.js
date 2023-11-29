eececf63080bbe8af0e234f98cf04ab04527bb11
vova3002 <98548237+vova3002@users.noreply.github.com>
2023-11-29 19:12:07 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,44 +1,76 @@
-const friends = ["Mango", "Poly", "Kiwi", "Ajax"]
-console.log(friends.join(", "))
+// // Напиши функцію logItems(array), яка отримує масив і використовує цикл for, який для кожного елемента масиву буде виводити в консоль повідомлення в форматі [номер елемента] - [значення елемента].
+// const logItems = [100, 200, 300, 400, 500];
+// for(let i = 0; i < logItems.length; i += 1) {
+//   console.log(`${i} ${logItems[i]}`);
+// };
 
-// join ^
-// for v
-let friendsFor = ""
-for(let i = 0; i < friends.length; i += 1){
-  friendsFor += friends[i] + ", "
-}
-console.log(friendsFor)
-
-// do 2 ways ^
-
-// // 1 ^
-// // 2 v
-
-const cards = [
-  'Карточка-1',
-  'Карточка-2',
-  'Карточка-3',
-  'Карточка-4',
-  'Карточка-5',
-  
-]
-
+// 1 ^
+// 2 v
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+// function calculateEngravingPrice(message, pricePerWord) {
+//   const array = message.split(" ")
+//   let quantityWords = array.length
+//   const totalPrice = quantityWords * pricePerWord
+//   console.log(totalPrice)
+// };
+// calculateEngravingPrice('Hello vova', 100)
 // 2 ^
 // 3 v
 
-const cardToRemove = cards.splice(2, 1)
-console.log(cards)
+// function findLongestWord(string) {
+// const arrayWords = string.split(' ')
+// let longestWord = arrayWords[0]
+// for(let i = 0; i < arrayWords.length; i += 1) {
+//   arrayWords[i].length
+//   if(longestWord.length < arrayWords[i].length) {
+//     longestWord = arrayWords[i]
+//   }
+// }
+// return longestWord
+// }
+// console.log(findLongestWord("This is the greatest message ever"))
+// console.log(findLongestWord("Vova is learning javascript"))
 
 // 3 ^
 // 4 v
 
-const cardToAdd = cards.splice(4, 0, "Карточка-6")
-console.log(cards)
+// function formatString(string) {
+//   const stringLength = string.length
+//   if (stringLength < 40) {
+//     return string
+//   } else {
+//     console.log(string.slice(0, 40))
+//   }
+// }
+// formatString('Learning javascript so I can make websites on google');
 
 // 4 ^
 // 5 v
 
-const cardToUpdate = cards.splice(1, 1, "Карточка-4")
-console.log(cards)
+// function checkForSpam(message) {
+// const messageCheck = message.split(' ')
+// if(messageCheck.includes("spam" || "sale")) {
+//   alert("There's a forbidden message on your text")
+// } else {
+//   alert("There's no forbidden message on your text")
+// }
+// }
+// checkForSpam("School is always spamming too much homework")
 
 // 5 ^
+// 6 v
+
+function sendNumber() {
+  const askArray = []
+  let askNumber
+  do{
+    askNumber = prompt("Введіть число")
+    askArray.push(askNumber)
+  } while(askNumber !== null)
+  return askArray
+}
+console.log(sendNumber())
+// 6 ^
+// 7 v
+
+// 7 ^
