be16ccdcb49b9a7256eb0052dcebcfa3b36db229
vova3002 <98548237+vova3002@users.noreply.github.com>
2023-12-30 14:55:31 +0200

    w
    as

--- js/index.js
+++ js/index.js
@@ -1,73 +1,76 @@
-// const buttonJS = document.querySelector("button")
-// buttonJS.addEventListener("click", () => {
-//   alert("Hello World!")
-// } );
+let bankAccount = {
+  ownerName: "Vova",
+  accountNumber: 782,
+  balance: 10000,
+  deposit() {
+    const askNum = Number(prompt("Введіть кількість грошей ви хочете поповнути банківський аккайунт."))
+    this.balance += askNum
+    return this.balance
+  },
+  withdraw(){
+    const askTakeNum = Number(prompt("Введіть кількість грошей ви хочете забрати з банківського аккайунту."))
+    this.balance -= askTakeNum
+    return this.balance
+  }
+}
+
+console.log(bankAccount.deposit())
+console.log(bankAccount.withdraw())
 
 // 1 ^
 // 2 v
+const currentTemperature = prompt("Введіть вашу температуру.")
+let weather = {
+  temperature: currentTemperature,
+  determinWeather() {
+    if(this.temperature < 0) {
+      return "true"
+    } else {
+      return "false"
+    }
+  },
+  humidity: 50,
+  windSpeed: 12,
+}
 
-// const input = document.querySelector("input");
-// const text = docuemnt.querySelector(".text1");
-// const textEnd = document.querySelector(".text2");
-// const button = document.querySelector("button");
-
-// const guessTheNumber = Number(prompt("Вгадайте число від 1 - 100"));
-
-// const guessNum = function(guesser) {
-//   let generatedNumbers = Math.random() * (100 - 1) + 1
-//   generatedNumbers = Math.round(generatedNumbers)
-//   if(guesser === generatedNumbers) {
-//     alert("Ви вгадали число!")
-//   }
-//   else {
-//     alert("Ви не вгадали число.")
-//   }
-// }
-// guessNum(guessTheNumber)
+console.log(weather.determinWeather())
 
 // 2 ^
 // 3 v
-// let counter = 0;
 
-// const buttonCheck = document.querySelector('.button3')
-// buttonCheck.addEventListener('click',()=> {
-//    counter += 1;
-// })
-// console.log(counter);
+let user = {
+  name: "vova",
+  email: "real.volodymir@gmail.com",
+  password: 12945,
+  login() {
+    const emailTest = prompt("Увійдіть свою пошту")
+    if(this.email === emailTest) {
+      alert("Ви увійшли!")
+    } else {
+      alert("Ви не увійшли.")
+    }
+  }
+}
+
+console.log(user.login())
 
 // 3 ^
 // 4 v
 
-// const applyCallbackToEachElement = (arr, callback) => {
-//   const resultForeach = []
-//   arr.forEach(element => resultForeach.push(callback(element)))
-//   const resultMap = arr.map(element => callback(element))
+let movie = {
+  title: "Wonka",
+  director: "Paul King",
+  year: 2023,
+  rating () {
+    let rateThisFilm = Number(prompt("Чи вам це ї фільм сподобався? Від 10 - 1"))
+    if(rateThisFilm > 7) {
+      return "true"
+    }else {
+      return "false"
+    }
+  }
+}
 
-//   console.log('resultForeach', resultForeach)
-//   console.log('resultMap', resultMap)
-
-//   return arr.foreach(element => callback(element))
-// }
-
-// const arr = [1, 2, 3, 4, 5];
-
-// const squareCallback = (elem) => elem * elem
-
-// const result = applyCallbackToEachElement(arr, squareCallback);
-// console.log(result)
+console.log(movie.rating())
 
 // 4 ^
-// 5 v
-// const calculateDiscountPrice = (price, discount, callback) => {
-//   const discounter = price - price * (discount / 100)
-//   callback(discounter)
-// }
-
-// let showDiscountPrice = (discountPrice) => {
-//   console.log(`Discounted price: ${discountPrice}`)
-// }
-
-// calculateDiscountPrice(100, 10, showDiscountPrice);
-
-
-// 5 ^
