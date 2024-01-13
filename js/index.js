d4fc0f7e14e5b360723959c6b02d89fc7f491f07
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-01-13 16:18:29 +0200

    annoyed

--- js/index.js
+++ js/index.js
@@ -1,76 +1,150 @@
-let bankAccount = {
-  ownerName: "Vova",
-  accountNumber: 782,
-  balance: 10000,
-  deposit() {
-    const askNum = Number(prompt("Введіть кількість грошей ви хочете поповнути банківський аккайунт."))
-    this.balance += askNum
-    return this.balance
-  },
-  withdraw(){
-    const askTakeNum = Number(prompt("Введіть кількість грошей ви хочете забрати з банківського аккайунту."))
-    this.balance -= askTakeNum
-    return this.balance
-  }
-}
+// const user = {
+//   mood: "happy",
+//   hobby: "videoGame",
+//   premium: "premium",
+//   changer() {
+//     this.hobby = "skydiving"
+//     this.premium = "false"
+//   }
+// }
 
-console.log(bankAccount.deposit())
-console.log(bankAccount.withdraw())
+// const keys = Object.keys(user)
 
-// 1 ^
-// 2 v
-const currentTemperature = prompt("Введіть вашу температуру.")
-let weather = {
-  temperature: currentTemperature,
-  determinWeather() {
-    if(this.temperature < 0) {
-      return "true"
-    } else {
-      return "false"
-    }
-  },
-  humidity: 50,
-  windSpeed: 12,
-}
+// for (const key of keys) {
+//   alert(`${key}: ${keys}`)
+// };
 
-console.log(weather.determinWeather())
+// console.log(keys)
 
-// 2 ^
+// 1
+// 2
+// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+
+// const countProps = (obj) => {
+// const arrayTurner = Object.keys(obj)
+// return arrayTurner.length
+// }
+
+// const object = {
+//   day: "bright",
+//   weather: "hot",
+//   working: true
+// }
+
+// countProps(object)
+// console.log(countProps(object))
+
+// console.log(array)
+// 2
 // 3 v
+// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
 
-let user = {
-  name: "vova",
-  email: "real.volodymir@gmail.com",
-  password: 12945,
-  login() {
-    const emailTest = prompt("Увійдіть свою пошту")
-    if(this.email === emailTest) {
-      alert("Ви увійшли!")
-    } else {
-      alert("Ви не увійшли.")
-    }
-  }
-}
+// const findBestEmployee = (employees) => {
+//   const converter = Object.values(employees)
+//   let bestEmployeer = 0
+//   converter.forEach((employeer) => {
+//     if(employeer > bestEmployeer) {
+//       bestEmployeer = employeer
+//     }
+//     return bestEmployeer
+//   })
+//   return bestEmployeer
+// }
 
-console.log(user.login())
+// const workerList = {
+//   danya: 5,
+//   vova: 2,
+//   michael: 7
+// }
+
+// findBestEmployee(workerList)
+// console.log(findBestEmployee(workerList))
 
 // 3 ^
 // 4 v
+// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
 
-let movie = {
-  title: "Wonka",
-  director: "Paul King",
-  year: 2023,
-  rating () {
-    let rateThisFilm = Number(prompt("Чи вам це ї фільм сподобався? Від 10 - 1"))
-    if(rateThisFilm > 7) {
-      return "true"
-    }else {
-      return "false"
+// const countTotalSalary = (employees) => {
+//   const workerArray = Object.values(employees)
+//   let collectSalary = 0;
+//   for (let i = 0; i < workerArray.length; i += 1) {
+//     collectSalary += workerArray[i]
+//   }
+//   return collectSalary
+// }
+
+// const salaryList = {
+//   worker1: 10000,
+//   worker2: 4000,
+//   worker3: 7000
+// }
+
+// countTotalSalary(salaryList)
+// console.log(countTotalSalary(salaryList))
+
+// // 4 ^
+// // 5 v
+// // Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.
+
+// const getAllPropValues = (arr, prop) => {
+//   for (const fruit of arr) {
+//     if(fruit.fruitName === prop) {
+//       const arrayKeys = Object.keys(fruit)
+//       console.log(arrayKeys)
+//       return arrayKeys
+//     }
+//   }
+// }
+
+// const fruitsArray = [
+//   {
+//     fruitName: "Lemon",
+//     price: 20,
+//   },
+//   {
+//     fruitName: "Apple",
+//     price: 25,
+//   },
+//   {
+//     fruitName: "Watermelon",
+//     price: 40
+//   }
+// ]
+
+// getAllPropValues(fruitsArray, "Apple")
+
+// 5 ^
+// 6 v
+// Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+let total = 0;
+const calculateTotalPrice = (allProdcuts, productName) => {
+  for (let i = 0; i < allProdcuts.length; i += 1) {
+    if(allProdcuts[i].productName === productName) {
+     total = allProdcuts[i].price + total
     }
   }
+  return total
 }
 
-console.log(movie.rating())
+const allProdcutsArray = [
+  {
+    productName: "apple", 
+    price: 28
+  },
+  {
+    productName: "bread",
+    price: 34
+  },
+  {
+    productName: "meat",
+    price: 45
+  },
+  {
+    productName: "apple",
+    price: 32
+  }
+]
+calculateTotalPrice(allProdcutsArray, "apple")
+console.log(calculateTotalPrice(allProdcutsArray(total)))
 
-// 4 ^
+// 6 ^
