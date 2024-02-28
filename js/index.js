68138d833d213a9924b36c513072185ee38b9d39
vova3002 <98548237+vova3002@users.noreply.github.com>
2024-02-28 19:34:04 +0200

    update

--- js/index.js
+++ js/index.js
@@ -18,50 +18,50 @@ backdropModal.addEventListener("click", (event) => {
 // 1 ^
 // 2 v
 
-// const redColor = document.getElementById('red')
-// const whiteColor = document.getElementById('white')
-// const greenColor = document.getElementById('green')
-// const formColor = document.getElementById('form')
+const redColor = document.getElementById('red')
+const whiteColor = document.getElementById('white')
+const greenColor = document.getElementById('green')
+const formColor = document.getElementById('form')
 
-// redColor.addEventListener('focus', (e) => {
-// formColor.style.backgroundColor = "red"
-// })
-// whiteColor.addEventListener('focus', (e) => {
-// formColor.style.backgroundColor = "white"
-// })
-// greenColor.addEventListener('focus', (e) => {
-// formColor.style.backgroundColor = "green"
-// })
+redColor.addEventListener('focus', (e) => {
+formColor.style.backgroundColor = "red"
+})
+whiteColor.addEventListener('focus', (e) => {
+formColor.style.backgroundColor = "white"
+})
+greenColor.addEventListener('focus', (e) => {
+formColor.style.backgroundColor = "green"
+})
 
 // 2 ^
 // 3 v
 
-// const text = document.getElementById('name-output')
-// const nameInputer = document.getElementById('name-input')
+const text = document.getElementById('name-output')
+const nameInputer = document.getElementById('name-input')
 
-// nameInputer.addEventListener('input', (e) => {
-//   text.textContent = (nameInputer.value)
-// })
+nameInputer.addEventListener('input', (e) => {
+  text.textContent = (nameInputer.value)
+})
 
 // 4 v
-// const input = document.getElementById("validation-input")
+const input = document.getElementById("validation-input")
 
-// input.addEventListener('input', (e) => {
-//   if(input.value.length < input.getAttribute("data-length")) {
-//     input.classList = ('valid')
-//     console.log(input)
-//   } else {
-//     input.classList = ('invalid')
-//     console.log(input)
-//   }
-// })
+input.addEventListener('input', (e) => {
+  if(input.value.length < input.getAttribute("data-length")) {
+    input.classList = ('valid')
+    console.log(input)
+  } else {
+    input.classList = ('invalid')
+    console.log(input)
+  }
+})
 
 // 5 v
 
-// const fontSize = document.getElementById("font-size-control");
-// const textMagic = document.getElementById("text");
+const fontSize = document.getElementById("font-size-control");
+const textMagic = document.getElementById("text");
 
-// fontSize.addEventListener("input", (e) => {
-//   textMagic.style.fontSize = fontSize.value + "px"
-//   console.log(textMagic)
-// })
+fontSize.addEventListener("input", (e) => {
+  textMagic.style.fontSize = fontSize.value + "px"
+  console.log(textMagic)
+})
